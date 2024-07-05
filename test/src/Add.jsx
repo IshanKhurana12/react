7ad50f667a3989104502a7ba70412803969f485a
Ishan Khurana <ishankhurana@Ishans-MacBook-Air.local>
2024-07-06 00:12:40 +0530

    adding new component to components branch after testing the component i will add it in main

---
+++ test/src/Add.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Add() {
+  return (
+    <div>
+      add 2 numbers
+    </div>
+  )
+}
--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Add from './Add'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     hello
+    <Add />
     </>
   )
 }
