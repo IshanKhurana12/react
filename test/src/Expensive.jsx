ae01f7982ef52b666778291fc133b0bd8701c610
Ishan Khurana <ishankhurana@Ishans-MacBook-Air.local>
2024-07-11 03:42:55 +0530

    use memo done

--- test/src/Expensive.jsx
+++ test/src/Expensive.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 export default function Expensive() {
     const [ans,setans]=useState(0);
 
-
+const [todo,addtodo]=useState([]);
     const exp=(num)=>{ 
             for(let i =0;i<1000000000;i++){
                 num+=1;
@@ -11,9 +11,11 @@ export default function Expensive() {
             return  num;
     } 
 
-   const calculation=useMemo(()=>exp(ans),[ans]) ; 
-   
+function add(){
+    addtodo([...todo,"new todo"]);
+}
 
+   const calculation=useMemo(()=>exp(ans),[ans]); 
 const increment=()=>{
     setans(ans+1);
 }
@@ -22,6 +24,10 @@ const increment=()=>{
     <div>
         expensive calculation value {calculation}
         <button onClick={increment}>increment counter</button>
+        <button onClick={add}>add todo</button>
+        {todo.map((t)=>(
+            <h1>{t}</h1>
+        ))}
         <h1>counter value {ans}</h1>
     </div>
   )
